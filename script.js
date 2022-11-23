4b9361c103e9ad64992e90923b4ef079054617e5
alejo742 <a.manrique742@gmail.com>
2022-11-23 06:52:24 -0500

    career almost finished

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-
+function toggleDisplay(element, value) {
+    if(element.style.display == 'none') {
+        element.style.display == value
+    }
+    else {
+        element.style.display = 'none'
+    }
+}
+function setAnimation(element, animationName, duration) {
+    element.style.animation = animationName +' '+ duration
+}
 
