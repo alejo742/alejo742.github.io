24ada97456f2af6f546c21565b9a0c5072828424
alejo742 <a.manrique742@gmail.com>
2022-11-24 06:39:52 -0500

    responsive remaining, page finished

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
-function toggleDisplay(element, value) {
-    if(element.style.display == 'none') {
-        element.style.display == value
+  //? animacion onscroll arriba:
+function revealUp() {
+    var reveals = document.querySelectorAll(".reveal-up");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 20;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active-up");
+      } else {
+        reveals[i].classList.remove("active-up");
+      }
     }
-    else {
-        element.style.display = 'none'
+  }
+window.addEventListener("scroll", revealUp);
+
+//? animacion onscroll derecha: 
+function revealRight() {
+    var reveals = document.querySelectorAll(".reveal-right");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 20;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active-right");
+      } 
+      else {
+        reveals[i].classList.remove("active-right");
+      }
     }
-}
-function setAnimation(element, animationName, duration) {
-    element.style.animation = animationName +' '+ duration
-}
+  }
+window.addEventListener("scroll", revealRight);
+
+//? animacion onscroll izquierda: 
+function revealLeft() {
+    var reveals = document.querySelectorAll(".reveal-left");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 20;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active-left");
+      } 
+      else {
+        reveals[i].classList.remove("active-left");
+      }
+    }
+  }
+window.addEventListener("scroll", revealLeft);
