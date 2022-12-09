073b67fc6c66a656559078379ff0e5a14abf6f88
alejo742 <a.manrique742@gmail.com>
2022-12-09 06:27:47 -0500

    denuncia responsive

--- IPCole/scripts/denuncia.js
+++ IPCole/scripts/denuncia.js
@@ -1,19 +1,9 @@
-var quiz1 = document.getElementById("quiz1")
-var quiz2 = document.getElementById("quiz2")
-var quiz3 = document.getElementById("quiz3")
-var title1 = document.getElementById("click-title1")
-var title2 = document.getElementById("click-title2")
-var title3 = document.getElementById("click-title3")
+function showSteps() {
+    document.querySelector('header').style.display = 'none'
+    document.querySelector('main').style.display ='flex'
+}
 
-function show1() {
-    quiz1.style.display = "block"
-    title1.style.display = "none"
-}
-function show2() {
-    quiz2.style.display = "grid"
-    title2.style.display = "none"
-}
-function show3() {
-    quiz3.style.display = "grid"
-    title3.style.display = "none"
+function showFinalMessage() {
+    document.querySelector('main').style.display ='none'
+    document.querySelector('section').style.display ='flex'
 }
